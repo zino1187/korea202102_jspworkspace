04beee73866118747e8123030a9344472166bcbb
zino1187 <zino1187@gmail.com>
2021-06-18 11:26:36 +0900

    c

---
+++ site0617/src/main/java/site0617/util/FileManager.java
@@ -0,0 +1,16 @@
+package site0617.util;
+
+public class FileManager {
+	
+	//확장자만 추출하기 , 경로를 넘겨받아 확장자만 추출
+	public static String getExt(String path) {
+		//test.kkk.jpg 
+		return path.substring(path.lastIndexOf(".")+1, path.length());
+	}
+	
+	/*
+	public static void main(String[] args) {
+		System.out.println(getExt("test.kkk.jpg"));
+	}
+	*/
+}

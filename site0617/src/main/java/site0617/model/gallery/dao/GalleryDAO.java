2793c35e9678523370eb3bef4fb829cb33227305
zino1187 <zino1187@gmail.com>
2021-06-18 13:13:00 +0900

    c

--- site0617/src/main/java/site0617/model/gallery/dao/GalleryDAO.java
+++ site0617/src/main/java/site0617/model/gallery/dao/GalleryDAO.java
@@ -1,19 +1,40 @@
 package site0617.model.gallery.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import site0617.model.domain.Gallery;
+import site0617.model.pool.PoolManager;
 
 //이 클래스는 오직 gallery에 테이블에 대한 CRUD만을 담당
 public class GalleryDAO {
+	PoolManager pool=PoolManager.getInstance();
 	
-	public void insert() {
+	public int insert(Gallery gallery) {
 		Connection con=null;
 		PreparedStatement pstmt=null;
+		int result=0;
 		
-		con=커넥션풀에서 가져오자;
+		con=pool.getConnection();
 		String sql="insert into gallery(gallery_id, title, writer, content, filename) values(seq_gallery.nextval,?,?,?,?)";
-		
+		try {
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, gallery.getTitle());
+			pstmt.setString(2, gallery.getWriter());
+			pstmt.setString(3, gallery.getContent());
+			pstmt.setString(4, gallery.getFilename());
+			result=pstmt.executeUpdate();//쿼리수행
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			pool.release(con, pstmt);
+		}
+		return result;
 	}
-	
 }
+
+
+
+
+

86d9f82f8ddbe77d75f34f8dc13ca05d5b93e621
zino1187 <zino1187@gmail.com>
2021-06-21 14:22:01 +0900

    c

--- site0617/src/main/java/site0617/photo/RegistServlet.java
+++ site0617/src/main/java/site0617/photo/RegistServlet.java
@@ -18,6 +18,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import site0617.model.domain.Gallery;
+import site0617.model.gallery.dao.GalleryDAO;
 import site0617.util.FileManager;
 
 //아파치 업로드 컴포넌트를 이용한 파일업로드 구현
@@ -25,11 +26,13 @@ public class RegistServlet extends HttpServlet{
 	ServletContext context;
 	String path;
 	int maxSize=2*1024*1024; //2M byte
+	GalleryDAO galleryDAO; 
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		context = config.getServletContext();
 		path = context.getRealPath("/data");
+		galleryDAO = new GalleryDAO();
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -51,8 +54,9 @@ public class RegistServlet extends HttpServlet{
 		
 		//업로드 처리 객체 
 		ServletFileUpload upload=new ServletFileUpload(factory);//설정 정보 적용하여 인스턴스 생성
-		Gallery gallery=new Gallery();
 		request.setCharacterEncoding("utf-8"); //파라미터에 대한 인코딩(한글 안깨지게)
+		
+		Gallery gallery=new Gallery();
 		
 		try {
 			List<FileItem> itemList=upload.parseRequest(request);
@@ -60,8 +64,17 @@ public class RegistServlet extends HttpServlet{
 			for( FileItem item : itemList) {
 				if(item.isFormField()) { //inpuyt type='text' 박스라면...
 					
-					out.print(item.getFieldName()+"필드의 값은 "+item.getString()+"<br>");
+					out.print(item.getFieldName()+"필드의 값은 "+item.getString("utf-8")+"<br>");
 					
+					String value = item.getString("utf-8");
+					
+					if(item.getFieldName().equals("title")) {//제목
+						gallery.setTitle(value);
+					}else if(item.getFieldName().equals("writer")) {//작성자
+						gallery.setWriter(value);
+					}else if(item.getFieldName().equals("content")) {//내용
+						gallery.setContent(value);
+					}
 				}else { //아니라면, 얘는 input type='file'
 					long time=System.currentTimeMillis();
 					String newName=item.getName(); //업로드된 파일명 반환  
@@ -69,7 +82,22 @@ public class RegistServlet extends HttpServlet{
 					
 					String ext=FileManager.getExt(newName);//확장자 구하기
 					item.write(new File(path+"/"+time+"."+ext)); //서버에 저장!!!
+					gallery.setFilename(time+"."+ext);
 				}
+				
+				//반복문이 종료되면, VO에 알맞는 데이터가 채워져 있다!!
+				//이 시점에 VO를 이용하여 insert 수행!!
+				int result = galleryDAO.insert(gallery);
+				out.print("<script>");
+				
+				if(result==0) {
+					out.print("alert('등록실패');");
+					out.print("history.back();");
+				}else {
+					out.print("alert('등록성공');");
+					out.print("location.href='/photo/list.jsp';");
+				}
+				out.print("</script>");
 			}
 		} catch (FileUploadException e) {
 			e.printStackTrace();

2780a4463aa3f5d14545511e86772c387613f5e9
zino1187 <zino1187@gmail.com>
2021-06-21 11:47:53 +0900

    c

--- site0617/src/main/java/site0617/photo/RegistServlet.java
+++ site0617/src/main/java/site0617/photo/RegistServlet.java
@@ -14,9 +14,12 @@ public class RegistServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//jsp에서의 page지시영역의 conentType과 동일
-		response.setCharacterEncoding("text/html;charset=utf-8"); 
+		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.print("저 실행성공이요~!");
+		
+		//아파치 파일 업로드를 이용한 파일업로드 처리~~
+		
 		
 	}
 }

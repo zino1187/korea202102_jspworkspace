2d1d5827d5b52721000ddb2de4569927ad40ca95
zino1187 <zino1187@gmail.com>
2021-06-24 11:19:56 +0900

    c

--- site0622/src/main/java/com/koreait/site0622/model/comments/dao/JdbcCommentsDAO.java
+++ site0622/src/main/java/com/koreait/site0622/model/comments/dao/JdbcCommentsDAO.java
@@ -1,14 +1,35 @@
 package com.koreait.site0622.model.comments.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.koreait.site0622.model.domain.Comments;
+import com.koreait.site0622.util.pool.PoolManager;
 
 public class JdbcCommentsDAO implements CommentsDAO{
+	PoolManager pool=PoolManager.getInstance();
 	
 	public int insert(Comments comments) {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int result=0;
+		
+		con = pool.getConnection();
+		String sql="insert into comments(msg, cwriter, news_id) values(?,?,?)";
+		try {
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, comments.getMsg());
+			pstmt.setString(2, comments.getCwriter());
+			pstmt.setInt(3, comments.getNews_id());
+			result=pstmt.executeUpdate();//쿼리수행
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			pool.release(con, pstmt);
+		}
+		return result;
 	}
 
 	@Override

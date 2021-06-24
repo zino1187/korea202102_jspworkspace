410fa1120dbd9f92e529f67b257434b0500b22f9
zino1187 <zino1187@gmail.com>
2021-06-24 16:09:21 +0900

    c

--- site0622/src/main/webapp/js/Comments.js
+++ site0622/src/main/webapp/js/Comments.js
@@ -3,7 +3,7 @@
 	2015 ECMA Script 에서부터는 js도 class를 지원!!!
  */
 class Comments{
-	constructor(){
+	constructor(container, msg, cwriter, cdate){
 		//구성요소 생성
 		this.div=document.createElement("div");	
 		this.t_msg=document.createElement("input"); //default 는 text field
@@ -17,6 +17,18 @@ class Comments{
 		this.t_msg.readOnly=true;
 		this.t_cdate.style.width=10+"%";
 		this.t_cdate.readOnly=true;
-		
+
+		//값 대입
+		this.t_msg.value=msg;
+		this.t_cwriter.value=cwriter;
+		this.t_cdate.value=cdate;
+
+		//조립 
+		this.div.appendChild(this.t_msg);
+		this.div.appendChild(this.t_cwriter);
+		this.div.appendChild(this.t_cwriter);
+
+		//컨테이너에 부착 
+		container.appendChild(this.div);
 	}	
 }

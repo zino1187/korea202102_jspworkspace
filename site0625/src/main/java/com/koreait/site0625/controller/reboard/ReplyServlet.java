158bd7590344eae838f0660e47c7d1750b15519a
zino1187 <zino1187@gmail.com>
2021-06-28 10:33:09 +0900

    c

--- site0625/src/main/java/com/koreait/site0625/controller/reboard/ReplyServlet.java
+++ site0625/src/main/java/com/koreait/site0625/controller/reboard/ReplyServlet.java
@@ -1,14 +1,27 @@
 package com.koreait.site0625.controller.reboard;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.koreait.site0625.model.domain.ReBoard;
+import com.koreait.site0625.model.reboard.dao.MybatisReBoardDAO;
+import com.koreait.site0625.model.reboard.dao.ReBoardDAO;
+import com.koreait.site0625.util.message.MessageObject;
+
 //답변 등록 요청을 처리하는 서블릿
 public class ReplyServlet extends HttpServlet{
+	ReBoardDAO reboardDAO;
+	MessageObject obj;
+	
+	public void init() throws ServletException {
+		reboardDAO = new MybatisReBoardDAO();
+		obj = new MessageObject();
+	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//답변에 필요한 파라미터 받기 
@@ -20,8 +33,32 @@ public class ReplyServlet extends HttpServlet{
 		String writer=request.getParameter("writer");
 		String content=request.getParameter("content");
 		
+		//vo에 담기 
+		ReBoard reboard = new ReBoard(); //empty vo
+		reboard.setTeam(team);
+		reboard.setStep(step);
+		reboard.setDepth(depth);
+		reboard.setTitle(title);
+		reboard.setWriter(writer);
+		reboard.setContent(content);
+		
 		//답변 등록을 위한 메서드 호출 (자리확보 + 답변등록)
+		int result = reboardDAO.reply(reboard);
 		
+		//출력처리 
+		response.setContentType("text/html/charset=utf-8");
+		PrintWriter out=response.getWriter();
 		
+		if(result==0) {
+			out.print(obj.getMsgBack("답변 등록 실패"));
+		}else {
+			out.print(obj.getMsgURL("답변 등록 성공", "/reboard/list.jsp"));
+		}
 	}
 }
+
+
+
+
+
+

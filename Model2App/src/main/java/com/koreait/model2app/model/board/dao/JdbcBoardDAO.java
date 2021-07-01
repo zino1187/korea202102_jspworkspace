2d6af492dfadd81f9a8ecd081e047c3c13b90cba
zino1187 <zino1187@gmail.com>
2021-07-01 17:35:51 +0900

    c

--- Model2App/src/main/java/com/koreait/model2app/model/board/dao/JdbcBoardDAO.java
+++ Model2App/src/main/java/com/koreait/model2app/model/board/dao/JdbcBoardDAO.java
@@ -1,15 +1,46 @@
 package com.koreait.model2app.model.board.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.koreait.model2app.model.domain.Board;
+import com.koreait.model2app.util.pool.PoolManager;
 
 public class JdbcBoardDAO implements BoardDAO{
+	PoolManager pool=PoolManager.getInstance();
 	
-	@Override
 	public List selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List list=new ArrayList();
+		
+		con=pool.getConnection();
+		String sql="select * from board order by board_id desc";
+		try {
+			pstmt=con.prepareStatement(sql);
+			rs=pstmt.executeQuery(); //쿼리수행
+			while(rs.next()) {
+				Board board = new Board();
+				board.setBoard_id(rs.getInt("board_id"));
+				board.setTitle(rs.getString("title"));
+				board.setWriter(rs.getString("writer"));
+				board.setContent(rs.getString("content"));
+				board.setRegdate(rs.getString("regdate"));
+				board.setHit(rs.getInt("hit"));
+				
+				list.add(board);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			pool.release(con, pstmt, rs);
+		}
+		return list;
 	}
 
 	@Override

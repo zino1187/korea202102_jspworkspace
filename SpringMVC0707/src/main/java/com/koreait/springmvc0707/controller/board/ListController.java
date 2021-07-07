d75022f9480831e8921f2a633de8a22d9ddffd64
zino1187 <zino1187@gmail.com>
2021-07-07 15:51:22 +0900

    c

--- SpringMVC0707/src/main/java/com/koreait/springmvc0707/controller/board/ListController.java
+++ SpringMVC0707/src/main/java/com/koreait/springmvc0707/controller/board/ListController.java
@@ -1,18 +1,41 @@
 package com.koreait.springmvc0707.controller.board;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.koreait.springmvc0707.model.board.service.BoardService;
+
+import lombok.Setter;
+
 //게시판 목록 요청을 처리하는 하위 컨트롤러
+@Setter
 public class ListController implements Controller{
+	
+	//스프링으로부터 주입 받자!!
+	private BoardService boardService; //null
 
 	//3단계, 4단계
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+		//3단계: 일시킨다 
+		List boardList=boardService.selectAll();
+		
+		System.out.println("결과는 "+boardList);
+		
 		return null;
 	}
 
 }
+
+
+
+
+
+
+
+
+

e34c65c7819dbef8d73d89354dce7e8d50f9d086
zino1187 <zino1187@gmail.com>
2021-07-21 16:57:08 +0900

    c

--- ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/CategoryController.java
+++ ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/CategoryController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.koreait.shoppingmall.domain.SubCategory;
 import com.koreait.shoppingmall.model.service.category.SubCategoryService;
 import com.koreait.shoppingmall.model.service.category.TopCategoryService;
 
@@ -38,14 +40,17 @@ public class CategoryController {
 	
 	//상위 카테고리의 상세내역 요청 처리 
 	@RequestMapping(value="/category/topdetail", method=RequestMethod.GET)
-	public String getTopDetail(int topcategory_id, Model model) {
+	@ResponseBody
+	public List<SubCategory> getTopDetail(int topcategory_id, Model model) {
 		//3단계: 일시키기 
 		List subList=subCategoryService.selectAllById(topcategory_id);
 		
 		//4단계: 결과저장 
-		model.addAttribute("subList", subList); //forwaring needed...
+		//model.addAttribute("subList", subList); //forwaring needed...
+		//jsp를 통해서가 아니라, 현재 요청 메서드에서 응답자체를 json, xml 등으로 처리할 수 있는 방법
+		//자바객체를 자동으로 json표기법으로 String화시켜 자동변환해주는 라이브러리를 스프링은 이용할수 있다..
 		
-		return "admin/category/sublist";
+		return subList;
 	}
 	
 }
--- ShoppingMall/src/main/webapp/WEB-INF/views/admin/category/sublist.jsp
+++ ShoppingMall/src/main/webapp/WEB-INF/views/admin/category/sublist.jsp
@@ -1,3 +1,4 @@
+<%@page import="com.google.gson.Gson"%>
 <%@page import="com.koreait.shoppingmall.domain.SubCategory"%>
 <%@page import="java.util.List"%>
 <%@ page contentType="text/html;charset=UTF-8"%>
@@ -6,4 +7,7 @@
 	List<SubCategory> subList=(List)request.getAttribute("sublist");
 
 	//List --> Json 데이터로 변환해서 클라이언트에게 응답정보로 전송 
+	
+	//방법1 - json string 을 직접 작성하는 법 
+	//방법2 - java객체  <-->  json 자동변환 라이브러리 Gson 
 %>

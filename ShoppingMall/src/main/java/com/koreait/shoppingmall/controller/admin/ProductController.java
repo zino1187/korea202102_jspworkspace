4a36e2e85bad30de64ed2f71f4e412587da20b9f
zino1187 <zino1187@gmail.com>
2021-07-22 17:26:54 +0900

    c

--- ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/ProductController.java
+++ ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/ProductController.java
@@ -1,6 +1,11 @@
 package com.koreait.shoppingmall.controller.admin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -32,13 +37,32 @@ public class ProductController {
 	
 	//상품 등록 요청(파일업로드가 포함되어 있슴)
 	@PostMapping("/product/regist")
-	public String regist(Product product) {
-		
+	public String regist(Product product, HttpServletRequest request) {
 		
 		//VO에 등록한 MultipartFile 객체에 업로드된 파일이 이미 들어있으므로, 개발자는 이 객체를 이용하여 업로드된 파일을
 		//원하는대로 제어하면 된다..
 		MultipartFile photo=product.getPhoto();
 		System.out.println("자동으로 업로드된 객체로 부터 얻은 파일명은 "+photo.getOriginalFilename());
+		
+		System.out.println("servlet context="+request.getServletContext());
+		ServletContext context = request.getServletContext();
+		String realPath=context.getRealPath("/resources/data");
+		long time=System.currentTimeMillis();
+		
+		String filename=photo.getOriginalFilename();
+		int lastIndex=filename.lastIndexOf(".");
+		String ext= filename.substring(lastIndex+1, filename.length()); //확장자 구하기
+		
+		//원하는 위치에 파일 저장하기
+		try {
+			photo.transferTo(new File(realPath+"/"+time+"."+ext));
+			System.out.println("최종 저장 경로"+realPath);
+			
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
